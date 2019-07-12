1c88d4a24b50d0bd5bbcd87b5b935a8df964aa65
sballesteros <sebastien.ballesteros@gmail.com>
2019-07-12 12:45:45 -0700

    make getPublishActionsBySlug couchdb 2.x friendly

--- src/views/get-publish-actions-by-slug.js
+++ src/views/get-publish-actions-by-slug.js
@@ -26,7 +26,25 @@ export default function getPublishActionsBySlug(slug, opts, callback) {
         return callback(err);
       }
 
-      callback(null, getDocs(body));
+      let payload = getDocs(body);
+
+      // Because of CouchDB 2.0 clustering the view may be out of date and
+      // miss some recent actions. We try to mitigate that here by recomputing
+      // the view from data from the store
+      if (store) {
+        // add current payload to store first
+        store.add(payload);
+        // reconstruct the payload from the store that may have more data
+        payload = store.getAll().filter(doc => {
+          return (
+            doc['@type'] === 'PublishAction' &&
+            doc.result &&
+            doc.result.slug === slug
+          );
+        });
+      }
+
+      callback(null, payload);
     }
   );
 }
